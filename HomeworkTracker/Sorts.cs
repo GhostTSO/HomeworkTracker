e540961ef549d40d92e29124eae7be12d714ad72
Caleb Maftei <calebmaftei15@gmail.com>
2019-11-20 18:22:17 -0700

    Created and wrote functiosn for Sort Class

---
+++ HomeworkTracker/Sorts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTracker
+{
+    class Sorts
+    {
+        public Sorts()
+        {
+            // No parameters, only a collection of functions
+        }
+
+        //This sort organizes each list by the most necessary assignment first.
+        public List<Assignment> mostEfficientSort(List<Assignment> assignments)
+        {
+            //Highest Number will be first... This assignment has the highest priority
+            assignments.OrderByDescending(t => (t.getPointValue()*t.getPriority())/(t.getTimeLeft().TotalHours));
+            return assignments;
+        }
+
+        public List<Assignment> deadLineSort(List<Assignment> assignments)
+        {
+            //The assignment with the longest time left will be the last in the list
+            assignments.OrderBy(t => t.getTimeLeft());
+            return assignments;
+        }
+
+        public List<Assignment> prioritySort(List<Assignment> assignments)
+        {
+            //The highest priority will be first, the lowest priority will be last
+            assignments.OrderByDescending(t => t.getPriority());
+            return assignments;
+        }
+    }
+}
